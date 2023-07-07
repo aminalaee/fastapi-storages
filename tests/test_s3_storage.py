7cdf14c0867c6ee1e4c0e99e8ddf1c42f5a0db74
Amin Alaee <mohammadamin.alaee@gmail.com>
2023-07-07 13:53:29 +0200

    Add `AWS_S3_CUSTOM_DOMAIN` and `AWS_QUERYSTRING_AUTH` (#28)

--- tests/test_s3_storage.py
+++ tests/test_s3_storage.py
@@ -9,36 +9,73 @@ from fastapi_storages import S3Storage
 os.environ["MOTO_S3_CUSTOM_ENDPOINTS"] = "http://custom.s3.endpoint"
 
 
+class PrivateS3Storage(S3Storage):
+    AWS_ACCESS_KEY_ID = "access"
+    AWS_SECRET_ACCESS_KEY = "secret"
+    AWS_S3_BUCKET_NAME = "bucket"
+    AWS_S3_ENDPOINT_URL = "custom.s3.endpoint"
+    AWS_S3_USE_SSL = False
+
+
 @mock_s3
 def test_s3_storage_methods(tmp_path: Path) -> None:
     s3 = boto3.client("s3")
-    s3.create_bucket(Bucket="test-bucket")
+    s3.create_bucket(Bucket="bucket")
 
     tmp_file = tmp_path / "example.txt"
     tmp_file.write_bytes(b"123")
-
-    class PrivateS3Storage(S3Storage):
-        AWS_ACCESS_KEY_ID = "access"
-        AWS_SECRET_ACCESS_KEY = "secret"
-        AWS_S3_BUCKET_NAME = "test-bucket"
-        AWS_S3_ENDPOINT_URL = "custom.s3.endpoint"
-        AWS_S3_USE_SSL = False
 
     storage = PrivateS3Storage()
 
     assert storage.get_name("test (1).txt") == "test_1.txt"
     assert (
         storage.get_path("test (1).txt")
-        == "http://custom.s3.endpoint/test-bucket/test_1.txt"
+        == "http://custom.s3.endpoint/bucket/test_1.txt"
     )
     assert (
-        storage.get_path("a/test.txt")
-        == "http://custom.s3.endpoint/test-bucket/a/test.txt"
+        storage.get_path("a/test.txt") == "http://custom.s3.endpoint/bucket/a/test.txt"
     )
     assert (
         storage.get_path("a/b/c/test.txt")
-        == "http://custom.s3.endpoint/test-bucket/a/b/c/test.txt"
+        == "http://custom.s3.endpoint/bucket/a/b/c/test.txt"
     )
 
     storage.write(tmp_file.open("rb"), "example.txt")
     assert storage.get_size("example.txt") == 3
+
+
+@mock_s3
+def test_s3_storage_querystring_auth(tmp_path: Path) -> None:
+    s3 = boto3.client("s3")
+    s3.create_bucket(Bucket="bucket")
+
+    tmp_file = tmp_path / "example.txt"
+    tmp_file.write_bytes(b"123")
+
+    class TestStorage(PrivateS3Storage):
+        AWS_QUERYSTRING_AUTH = True
+
+    storage = TestStorage()
+
+    assert storage.get_path("test.txt").startswith(
+        "http://custom.s3.endpoint/bucket/test.txt?"
+    )
+    assert storage.get_path("test.txt").count("AWSAccessKeyId=access") == 1
+    assert storage.get_path("test.txt").count("Signature=") == 1
+    assert storage.get_path("test.txt").count("Expires=") == 1
+
+
+@mock_s3
+def test_s3_storage_custom_domain(tmp_path: Path) -> None:
+    s3 = boto3.client("s3")
+    s3.create_bucket(Bucket="bucket")
+
+    tmp_file = tmp_path / "example.txt"
+    tmp_file.write_bytes(b"123")
+
+    class TestStorage(PrivateS3Storage):
+        AWS_S3_CUSTOM_DOMAIN = "s3.fastapi.storages"
+
+    storage = TestStorage()
+
+    assert storage.get_path("test.txt") == "http://s3.fastapi.storages/test.txt"
