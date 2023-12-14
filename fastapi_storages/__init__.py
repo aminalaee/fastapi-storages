173441f383ad8f5dfbcdaa5614a79f725a6ca27e
Amin Alaee <mohammadamin.alaee@gmail.com>
2023-12-14 11:46:01 +0100

    Version 0.2.1 (#38)

--- fastapi_storages/__init__.py
+++ fastapi_storages/__init__.py
@@ -2,7 +2,7 @@ from .base import StorageFile, StorageImage
 from .filesystem import FileSystemStorage
 from .s3 import S3Storage
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = [
     "FileSystemStorage",
     "S3Storage",
